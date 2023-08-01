e2ce45697bcd34448e7dd221fba57a4426193291
edmaala <edward.dilao.maala@gmail.com>
2023-08-02 00:37:34 +0800

    Add eslint prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,29 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'xo',
-		'plugin:react/recommended',
-	],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [
-				'.eslintrc.{js,cjs}',
-			],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	ignorePatterns: ['vite.config.ts', 'vite-env.d.ts'],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-	],
-	rules: {
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['xo', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      extends: ['xo-typescript'],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  ignorePatterns: ['vite.config.ts', 'vite-env.d.ts'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
 };
